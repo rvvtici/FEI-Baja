d46320253d63ed9504e25755041d382503577bb5
ricardoaufe <rasf0831@gmail.com>
2026-08-04 18:18:17 -0300

    add User, Category, and Item models with admin registration

--- backend/stock/models.py
+++ backend/stock/models.py
@@ -10,3 +10,37 @@ class Pessoa(models.Model):
         #python3 manage.py migrate -> rodar models p/ banco de dados
         # area admin
 
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.username
+    
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Item(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    qty = models.IntegerField(default=0)
+    minimum_qty = models.IntegerField(default=0)
+
+    barcode = models.CharField(max_length=50, unique = True,blank=True, null=True)
+
+    @property
+    def status(self):
+        if self.qty <= self.minimum_qty:
+            return 'Comprar'
+        if self.qty <= (self.minimum_qty * 1.20): # 20% a mais do mínimo (passível de alteração)
+            return 'Atenção'
+        return 'OK'
+    
+    def __str__(self) -> str:
+        return self.name
