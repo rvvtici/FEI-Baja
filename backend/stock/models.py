48707f1a54c866e87dd363002dd5699f62bd8b41
ricardoaufe <rasf0831@gmail.com>
2026-08-06 17:29:27 -0300

    refactor Item model and add ItemMovement model with validations

--- backend/stock/models.py
+++ backend/stock/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=50)
@@ -29,7 +30,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    qty = models.IntegerField(default=0)
+    qty = models.PositiveIntegerField(default=0)
     minimum_qty = models.IntegerField(default=0)
 
     barcode = models.CharField(max_length=50, unique = True,blank=True, null=True)
@@ -44,3 +45,31 @@ class Item(models.Model):
     
     def __str__(self) -> str:
         return self.name
+
+class ItemMovement(models.Model):  
+    class Types(models.TextChoices):
+        IN = 'IN', 'Entrada'
+        OUT = 'OUT', 'Saída'
+
+    class Reasons(models.TextChoices):
+        USAGE = 'USAGE', 'Uso Interno'
+        LOSS = 'LOSS', 'Perda'
+        BREAKAGE = 'BREAKAGE', 'Quebra'
+        EXCHANGE = 'EXCHANGE', 'Troca'
+        PURCHASE = 'PURCHASE', 'Compra'
+        EXPIRED = 'EXPIRED', 'Vencido'
+
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=3, choices=Types.choices)
+    reason = models.CharField(max_length=10, choices=Reasons.choices, default=Reasons.USAGE)
+    quantity = models.IntegerField()
+    observations = models.TextField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def clear(self):
+        if self.type == self.Types.OUT and self.quantity > self.item.qty:
+            raise ValidationError("Não é possível realizar a saída, pois a quantidade é maior que o estoque atual.")
+        
+    def __str__(self) -> str:
+        return f"{self.get_type_display()} | {self.item.name} | Qtd: {self.quantity}"
