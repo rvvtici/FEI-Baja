74b990b5750e5407467d16018e4e614fa9662761
ricardoaufe <rasf0831@gmail.com>
2026-08-06 18:35:16 -0300

    remove User model from admin + migration

--- backend/stock/admin.py
+++ backend/stock/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import Pessoa, User, Category, Item
 
 admin.site.register(Pessoa)
-admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Item)
