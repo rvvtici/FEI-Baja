b98ac755d4507ca358587041cd07be152b14190c
ricardoaufe <rasf0831@gmail.com>
2026-08-07 11:17:57 -0300

    add JWT authentication + update URL routing for stock API

--- backend/setup/urls.py
+++ backend/setup/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stock/', include('stock.urls')),
+    path('', include('stock.urls')),
 ]
--- backend/stock/urls.py
+++ backend/stock/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import ItemViewSet, ItemMovementViewSet
 from django.contrib import admin
 
@@ -16,7 +17,9 @@ router.register(r'categorias', views.CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('ferramentas', views.view_ferramentas, name="view_ferramentas"),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('admin/', admin.site.urls)
 
 ]
